0c92710a2c4263b19aca182dee705cb30537d628
viktorhungary <69192180+viktorhungary@users.noreply.github.com>
2021-10-19 17:23:53 +0200

    commit

---
+++ teszt.py
@@ -0,0 +1,10 @@
+x = int(input("Kérek egy egész számot: "))
+if x >= 80:
+    print("Jeles")
+elif x >= 60:
+    print("Jó")
+elif x>= 40:
+    ("Közepes")
+elif x >= 20:
+    print("Elégséges")
+else: print("elégetlen")
